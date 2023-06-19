44e40d6db40edabd83df0e181d827717f2ac291f
juu124 <juu124@dkitec.com>
2023-06-19 14:54:44 +0900

    [추가] 웹뷰 기능 추가

--- app/src/main/java/com/dki/hybridapptest/MainActivity.java
+++ app/src/main/java/com/dki/hybridapptest/MainActivity.java
@@ -3,14 +3,23 @@ package com.dki.hybridapptest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    WebView myWebView;
+    WebSettings myWebSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebSettings = myWebView.getSettings();
 
+        myWebSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://m.naver.com/");
     }
 }
