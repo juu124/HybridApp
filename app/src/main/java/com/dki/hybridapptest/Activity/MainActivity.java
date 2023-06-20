1f2062ead3c747cf429f94c1901d7b8662c8c5f6
juu124 <juu124@dkitec.com>
2023-06-20 16:54:24 +0900

    [추가] 전화번호, 이메일 클릭시 액션 추가

--- app/src/main/java/com/dki/hybridapptest/Activity/MainActivity.java
+++ app/src/main/java/com/dki/hybridapptest/Activity/MainActivity.java
@@ -1,18 +1,26 @@
 package com.dki.hybridapptest.Activity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dki.hybridapptest.Interface.WebAppInterface;
 import com.dki.hybridapptest.R;
+import com.dki.hybridapptest.Util.GLog;
 
 public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
     private WebSettings myWebSettings;
     private String webURL;
+    private Intent action;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +34,29 @@ public class MainActivity extends AppCompatActivity {
         myWebSettings.setJavaScriptEnabled(true);
         myWebView.addJavascriptInterface(new WebAppInterface(this), "DKITec");
         myWebView.loadUrl(webURL);
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                if (url.length() == 0 && url.equals("")) {
+                    GLog.d("url is null");
+                } else {
+                    if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        GLog.d("CALL_PHONE PERMISSION_DENIED 입니다.");
+                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 10);
+                    } else {
+                        if (url.toLowerCase().contains("tel:010".toLowerCase())) {
+                            action = new Intent(Intent.ACTION_CALL, Uri.parse(url));
+                            startActivity(action);
+                        }
+                    }
+                    if (url.toLowerCase().contains("mailto:".toLowerCase())) {
+                        action = new Intent(Intent.ACTION_SENDTO, Uri.parse(url));
+                        startActivity(action);
+                    }
+                }
+                return true;
+            }
+        });
     }
 }
