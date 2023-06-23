920234418011363cd20a1e18588736879b44a3dd
juu124 <juu124@dkitec.com>
2023-06-23 14:57:38 +0900

    [추가] 네이티브에서 웹으로 nativeToWebWithMsg(msg) 호출 추가

--- app/src/main/java/com/dki/hybridapptest/Interface/WebAppInterface.java
+++ app/src/main/java/com/dki/hybridapptest/Interface/WebAppInterface.java
@@ -2,11 +2,13 @@ package com.dki.hybridapptest.Interface;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.dki.hybridapptest.Activity.HelloWorldActivity;
@@ -42,5 +44,33 @@ public class WebAppInterface {
         } else {
             Toast.makeText(mContext, word, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @JavascriptInterface
+    public void showDialog() {
+        EditText editText = new EditText(mContext);
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle("Alert")
+                .setView(editText)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(mContext, "다이얼로그 OK 버튼 누름", Toast.LENGTH_SHORT).show();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                mWebView.loadUrl("javascript:nativeToWebWithMsg('" + String.valueOf(editText.getText()) + "')");
+                            }
+                        });
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(mContext, "다이얼로그 NO 버튼 누름", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
