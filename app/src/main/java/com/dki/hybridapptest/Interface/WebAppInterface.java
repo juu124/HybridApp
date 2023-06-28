418568d5b25e2b4c184658a02f255dd15ed6deb1
juu124 <juu124@dkitec.com>
2023-06-28 18:03:17 +0900

    [추가] 다이얼로그 커스텀 다이얼로그로 변경

--- app/src/main/java/com/dki/hybridapptest/Interface/WebAppInterface.java
+++ app/src/main/java/com/dki/hybridapptest/Interface/WebAppInterface.java
@@ -1,13 +1,15 @@
 package com.dki.hybridapptest.Interface;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -49,32 +51,38 @@ public class WebAppInterface {
         }
     }
 
+
     @JavascriptInterface
     public void showDialog() {
-        EditText editText = new EditText(mContext);
-        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-        builder.setTitle("Alert")
-                .setView(editText)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        Dialog dialog = new Dialog(mContext);
+        dialog.setContentView(R.layout.dialog_edit_view);
+        EditText dialogEditText = dialog.findViewById(R.id.dialog_edit);
+        Button dialogNoBtn = dialog.findViewById(R.id.noButton);
+        ;
+        Button dialogYesBtn = dialog.findViewById(R.id.yesButton);
+        ;
+
+        dialogNoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialogYesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.post(new Runnable() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(mContext, "다이얼로그 OK 버튼 누름", Toast.LENGTH_SHORT).show();
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                mWebView.loadUrl("javascript:nativeToWebWithMsg('" + String.valueOf(editText.getText()) + "')");
-                            }
-                        });
-                    }
-                })
-                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(mContext, "다이얼로그 NO 버튼 누름", Toast.LENGTH_SHORT).show();
+                    public void run() {
+                        String text = dialogEditText.getText().toString();
+                        mWebView.loadUrl("javascript:nativeToWebWithMsg('" + text + "')");
+                        dialog.dismiss();
                     }
                 });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
+            }
+        });
+        dialog.show();
     }
 
     @JavascriptInterface
--- app/src/main/java/com/dki/hybridapptest/adapters/RvUserListAdapter.java
+++ app/src/main/java/com/dki/hybridapptest/adapters/RvUserListAdapter.java
@@ -1,8 +1,10 @@
 package com.dki.hybridapptest.adapters;
 
+import android.app.Dialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,22 +19,68 @@ import com.dki.hybridapptest.utils.GLog;
 import java.util.ArrayList;
 
 public class RvUserListAdapter extends RecyclerView.Adapter<RvUserListAdapter.ViewHolder> {
-    ArrayList<UserResponse> mUserList = new ArrayList<>();
+    private ArrayList<UserResponse> mUserList = new ArrayList<>();
+
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        Dialog dialog;
         ImageView userAvatar;
+        TextView userIdType;
         TextView userId;
+        TextView userName;
         TextView userFirstName;
         TextView userLastName;
+        TextView userEmailType;
         TextView userEmail;
+
+        public void showDialog() {
+            dialog.show();
+            TextView userInfo;
+            Button dialogNoBtn;
+            Button dialogYesBtn;
+
+            userInfo = dialog.findViewById(R.id.user_info);
+            dialogNoBtn = dialog.findViewById(R.id.noButton);
+            dialogYesBtn = dialog.findViewById(R.id.yesButton);
+
+//            userInfo.setText(RetrofitApiManager.getInstance().requestGetUser(););
+
+            dialogNoBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            dialogYesBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+        }
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             userAvatar = itemView.findViewById(R.id.item_user_avatar);
-            userId = itemView.findViewById(R.id.item_user_id);
-            userFirstName = itemView.findViewById(R.id.item_user_first_name);
-            userLastName = itemView.findViewById(R.id.item_user_last_name);
-            userEmail = itemView.findViewById(R.id.item_user_email);
+            userIdType = itemView.findViewById(R.id.item_user_id_type);
+            userId = itemView.findViewById(R.id.item_user_id_tv);
+            userName = itemView.findViewById(R.id.item_user_name_type);
+            userFirstName = itemView.findViewById(R.id.item_user_first_name_tv);
+            userLastName = itemView.findViewById(R.id.item_user_last_name_tv);
+            userEmailType = itemView.findViewById(R.id.item_user_email_type);
+            userEmail = itemView.findViewById(R.id.item_user_email_tv);
+
+            dialog = new Dialog(itemView.getContext());
+            dialog.setContentView(R.layout.dialog_view);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    GLog.d("아이템을 클릭했습니다.");
+                    showDialog();
+                }
+            });
         }
     }
 
@@ -45,12 +93,15 @@ public class RvUserListAdapter extends RecyclerView.Adapter<RvUserListAdapter.Vi
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         GLog.d("onBindViewHolder");
+        holder.userIdType.getText();
         holder.userId.setText(mUserList.get(position).getId());
         Glide.with(holder.itemView)
                 .load(mUserList.get(position).getAvatar())
                 .into(holder.userAvatar);
+        holder.userName.getText();
         holder.userFirstName.setText(mUserList.get(position).getFirstName());
-        holder.userLastName.setText(mUserList.get(position).getLastName());
+        holder.userLastName.setText(" " + mUserList.get(position).getLastName());
+        holder.userEmailType.getText();
         holder.userEmail.setText(mUserList.get(position).getEmail());
     }
 
